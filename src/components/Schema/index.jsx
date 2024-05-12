270802503f8276f35761f6cac09dfd87f7df0224
jasvinderkhera <ankitkhera15@gmail.com>
2024-05-12 21:57:32 +0530

    validation and username field added

--- src/components/Schema/index.jsx
+++ src/components/Schema/index.jsx
@@ -1,6 +1,15 @@
 import * as Yup from "yup";
 
 export const signUpSchema = Yup.object({
+  username: Yup.string()
+  .min(3, "userame must be at least 3 characters long")
+  .max(20, "userame must be at most 20 characters long")
+  .matches(
+    /^[a-zA-Z0-9_-]$/,
+    "username must contain only alphabetic characters, numbers, underscore and hyphen "
+  )
+  .required("Please enter your username"),
+  
   name: Yup.string()
     .min(3, "Name must be at least 3 characters long")
     .max(20, "Name must be at most 20 characters long")
@@ -13,19 +22,22 @@ export const signUpSchema = Yup.object({
     .matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please enter a valid email address")
     // .email("Please enter a valid email address")
     .required("Please enter your email"),
-  password: Yup.string()
-  .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
-      "Password must contain at least one lowercase letter, one uppercase letter, one number, one special character, and be at least 6 characters long"
-  ),
 
-//   password: Yup.string()
-//     .matches(
-//       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]$/,
-//       "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
-//     )
-//     .min(6, "Password must be at least 6 characters")
-//     .required("Please enter your password"),
+
+  // password: Yup.string()
+  // .matches(
+  //     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+  //     "Password must contain at least one lowercase letter, one uppercase letter, one number, one special character, and be at least 6 characters long"
+  // ),
+
+  password: Yup.string()
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]$/,
+      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+    )
+    .min(6, "Password must be at least 6 characters")
+    .required("Please enter your password"),
+
   confirmPassword: Yup.string()
     .required()
     .oneOf([Yup.ref("password"), null], "Passwords must match"),
--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -2,10 +2,10 @@ import Footer from "../common/footer/footer";
 import NavBar from "../common/navBar/navBar";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { useFormik } from 'formik';
+import { useFormik } from "formik";
 import { signUpSchema } from "../Schema";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye, faEyeSlash } from '@fortawesome/fontawesome-free-solid';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/fontawesome-free-solid";
 import "./register.css";
 
 function Register() {
@@ -15,16 +15,16 @@ function Register() {
     name: "",
     email: "",
     password: "",
-    confirmPassword: ""
+    confirmPassword: "",
   };
 
   const formik = useFormik({
     initialValues: initialValues,
     validationSchema: signUpSchema,
     onSubmit: function (values, action) {
-      console.log("Values: ", values)
-      action.resetForm()
-    }
+      console.log("Values: ", values);
+      action.resetForm();
+    },
   });
 
   const togglePasswordVisibility = () => {
@@ -44,71 +44,102 @@ function Register() {
 
         <form className="container form " onSubmit={formik.handleSubmit}>
           <div className="contactus">
-            <h4>Contact Us</h4>
+            <h4>Registration</h4>
           </div>
 
-    <div>
-          <label htmlFor="name">Name</label>
-          <input
-            type='text'
-            className="border  w-100 p-2 "
-            id="name"
-            name="name"
-            placeholder="Enter your Name"
-            value={formik.values.name}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          </div>
-          {formik.errors.name && formik.touched.name ? (<p className="form-error">{formik.errors.name}</p>) : null}
-
-          <label htmlFor="email">Email  address</label>
-
-          <input
-            type="email"
-            className="border  w-100 p-2 "
-            id="email"
-            name="email"
-            placeholder="Enter your email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          {formik.errors.email && formik.touched.email ? (<p className="form-error">{formik.errors.email}</p>) : null}
-
-          <label htmlFor="password">Password</label>
-          <div className="position-relative">
+          <div>
+            <label htmlFor="username">User Name</label>
             <input
-              type={showPassword ? "text" : "password"}
-              autoComplete="new-password"
-              className="border  w-100 p-2  "
-              id="password"
-              name="password"
-              placeholder="Enter your password"
-              value={formik.values.password}
+              type="text"
+              className="border  w-100 p-2 "
+              id="username"
+              name="username"
+              placeholder="Enter your User Name"
+              value={formik.values.username}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
             />
-            <FontAwesomeIcon
-              icon={showPassword ? faEye : faEyeSlash}
-              className="eye-position"
-              onClick={togglePasswordVisibility}
-            />
-            {formik.errors.password && formik.touched.password ? (<p className="form-error">{formik.errors.password}</p>) : null}
           </div>
-          <label htmlFor="confirmPassword">Confirm Password</label>
+          {formik.errors.username && formik.touched.username ? (
+            <p className="form-error">{formik.errors.username}</p>
+          ) : null}
 
-          <input
-            type="password"
-            className="border  w-100 p-2 "
-            id="confirmPassword"
-            name="confirmPassword"
-            placeholder="Confirm your password"
-            value={formik.values.confirmPassword}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          {formik.errors.confirmPassword && formik.touched.confirmPassword ? (<p className="form-error">{formik.errors.confirmPassword}</p>) : null}
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              className="border  w-100 p-2 "
+              id="name"
+              name="name"
+              placeholder="Enter your Name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+          </div>
+          {formik.errors.name && formik.touched.name ? (
+            <p className="form-error">{formik.errors.name}</p>
+          ) : null}
+
+          <div>
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              className="border  w-100 p-2 "
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+          </div>
+          {formik.errors.email && formik.touched.email ? (
+            <p className="form-error">{formik.errors.email}</p>
+          ) : null}
+
+          <div>
+            <label htmlFor="password">Password</label>
+            <div className="position-relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                autoComplete="new-password"
+                className="border  w-100 p-2  "
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              <FontAwesomeIcon
+                icon={showPassword ? faEye : faEyeSlash}
+                className="eye-position"
+                onClick={togglePasswordVisibility}
+              />
+            </div>
+          </div>
+          {formik.errors.password && formik.touched.password ? (
+            <p className="form-error">{formik.errors.password}</p>
+          ) : null}
+
+          <div>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+
+            <input
+              type="password"
+              className="border  w-100 p-2 "
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="Confirm your password"
+              value={formik.values.confirmPassword}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+          </div>
+          {formik.errors.confirmPassword && formik.touched.confirmPassword ? (
+            <p className="form-error">{formik.errors.confirmPassword}</p>
+          ) : null}
 
           <button type="submit" className="btn btn-primary button">
             Register
@@ -123,7 +154,6 @@ function Register() {
             </p>
           </div>
         </form>
-
       </div>
 
       <Footer />
