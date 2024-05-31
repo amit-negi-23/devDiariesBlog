57c2bd7aa8d5b1f3d5f6d50751c2198099b0e2a8
amit-negi-23 <amitnegiit23@gmail.com>
2024-05-31 18:52:42 +0530

    alert msg added

--- src/components/common/navBar/navBar.jsx
+++ src/components/common/navBar/navBar.jsx
@@ -4,98 +4,114 @@ import search from "../../../assets/images/search-icon.png";
 
 function NavBar() {
   return (
-<nav className="navbar navbar-expand-lg">
-  <div className="container">
-  <button
-  // added bg-white to set background color to white
-  className="navbar-toggler border-0 bg-white" 
-  type="button"
-  data-bs-toggle="collapse"
-  data-bs-target="#navbarText"
-  aria-controls="navbarText"
-  aria-expanded="false"
-  aria-label="Toggle navigation"
->
-  <span className="navbar-toggler-icon"></span> 
-   
-</button>
-    <Link className="navbar-brand text-white d-none d-lg-block" to="#">
-      DevDiaries
-    </Link>
-    <div
-      className="collapse navbar-collapse justify-content-center"
-      id="navbarText"
-    >
-      <ul className="navbar-nav mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link active text-white" to="/sport">
-            Sport
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link text-white" to="/health">
-            Health
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link text-white" to="/political">
-            Political
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link text-white" to="/business">
-            Business
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link text-white" to="/finance">
-            Finance
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link text-white" to="/life">
-            Life
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link text-white" to="/entertainment">
-            Entertainment
-          </Link>
-        </li>
-      </ul>
-    </div>
-    <div className="d-flex align-items-center ms-auto d-block">
-    <img
-        src={search}
-        alt="search-icon"
-        style={{ width: "20px", height: "20px" }}
-        className="mx-2 my-1  d-lg-block"
-      />
-      <span className="text-white mx-2 fw-light ">|</span>
+    <nav className="navbar navbar-expand-lg">
+      <div className="container">
+        <button
+          // added bg-white to set background color to white
+          className="navbar-toggler border-0 bg-white"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
 
-      <Link
-        className="text-white mx-2 fw-light text-decoration-none fs-6"
-        to="/login"
-      >
-        Login
-      </Link>
-      <Link
-        className="text-white mx-2 fw-light text-decoration-none fs-6"
-        to="/register"
-      >
-        Register
-      </Link>
-    </div>
-    <div className="btn btn-outline-primary ms-3">
-    <Link
-        className="text-white mx-2 fw-light text-decoration-none fs-6"
-        to="/login"
-      >
-        + Create Post
-      </Link>
-    </div>
-  </div>
-</nav>
+        </button>
+        <Link className="navbar-brand text-white d-none d-lg-block" to="#">
+          DevDiaries
+        </Link>
+        <div
+          className="collapse navbar-collapse justify-content-center"
+          id="navbarText"
+        >
+          <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active text-white" to="/sport">
+                Sport
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/health">
+                Health
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/political">
+                Political
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/business">
+                Business
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/finance">
+                Finance
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/life">
+                Life
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/entertainment">
+                Entertainment
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="d-flex align-items-center ms-auto d-block">
+          <img
+            src={search}
+            alt="search-icon"
+            style={{ width: "20px", height: "20px" }}
+            className="mx-2 my-1  d-lg-block"
+          />
+          <span className="text-white mx-2 fw-light ">|</span>
+
+          <Link
+            className="text-white mx-2 fw-light text-decoration-none fs-6"
+            to="/login"
+          >
+            Login
+          </Link>
+          <Link
+            className="text-white mx-2 fw-light text-decoration-none fs-6"
+            to="/register"
+          >
+            Register
+          </Link>
+        </div>
+        <div className=" ms-3">
+          <button className="btn btn-outline-primary text-white " data-bs-toggle="modal" data-bs-target="#exampleModal">+ Create Post</button>
+        </div>
+
+        <div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div className="alert alert-primary">
+                <div class="modal-header p-0">
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body fs-5 fw-bold text-center p-2">
+                  You need to login first !
+                </div>
+              </div>
+              <div class="modal-footer border-top-0 p-0">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <Link to="/login">
+                  <button type="button" class="btn btn-primary " data-bs-dismiss="modal">okay!</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
 
   );
 }
