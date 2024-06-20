af59dee0b439bfcad74c0167a1432aaccd3b0d58
jasvinderkhera <ankitkhera15@gmail.com>
2024-06-20 16:49:04 +0530

    css for text-editor added

--- src/components/createPost/richTextEditor.jsx
+++ src/components/createPost/richTextEditor.jsx
@@ -1,35 +1,35 @@
 import { useState } from "react";
 import ReactQuill from "react-quill";
+import "./richTextEditor.css";
 import "react-quill/dist/quill.snow.css";
 
 function RichTextEditor() {
   const [text, setText] = useState("");
 
-  const publish=()=>{
-    console.log(text)
-setText("")
-  }
-
+  const publish = () => {
+    console.log(text);
+    setText("");
+  };
 
   const modules = {
     toolbar: [
-        [{ header: "1" }, { header: "2" }, { font: [] }],
-        [{ size: [] }],
-        ["bold", "italic", "underline", "strike", "blockquote"],
-        [{ color: [] }, { background: [] }],
-        [{ align: [] }],
-        [
-          { list: "ordered" },
-          { list: "bullet" },
-          { indent: "-1" },
-          { indent: "+1" },
-        ],
+      [{ header: "1" }, { header: "2" }, { font: [] }],
+      [{ size: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ color: [] }, { background: [] }],
+      [{ align: [] }],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
 
-        ["link", "image", "video"],
-        [{ script: "sub" }, { script: "super" }],
-        ["code-block"],
-        ["clean"],
-      ]
+      ["link", "image", "video"],
+      [{ script: "sub" }, { script: "super" }],
+      ["code-block"],
+      ["clean"],
+    ],
   };
 
   const formats = [
@@ -54,16 +54,33 @@ setText("")
     "code-block",
   ];
   return (
-    <>
-    <ReactQuill
-      theme="snow"
-      value={text}
-      onChange={setText}
-      modules={modules}
-      formats={formats}
-    />
-    <button type="button" onClick={publish}>Publish</button>
-    </>
+    <div className="container">
+      <div className="quill_title form-floating ">
+        <input
+          type="email"
+          className="form-control "
+          id="floatingInput"
+          placeholder="name@example.com"
+        />
+        <label for="floatingInput">Title</label>
+      </div>
+      <div className="quill-container">
+        <ReactQuill
+          theme="snow"
+          value={text}
+          onChange={setText}
+          modules={modules}
+          formats={formats}
+        />
+        <button
+          type="button"
+          onClick={publish}
+          className="btn btn-primary editor-btn"
+        >
+          Publish
+        </button>
+      </div>
+    </div>
   );
 }
 
