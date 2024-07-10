7a2439b4bf50a5f0a16b26b2f3bbf5af3a11f1fd
jasvinderkhera <ankitkhera15@gmail.com>
2024-07-10 20:26:48 +0530

    post list UI

--- src/components/myBlog/myBlog.jsx
+++ src/components/myBlog/myBlog.jsx
@@ -1,4 +1,6 @@
 import { useAppContext } from "../../contextApi/context";
+import pp from './images/profile.png'
+import './myBlog.css'
 
 function MyBlog() {
   const {
@@ -14,33 +16,100 @@ function MyBlog() {
             This blog page belong to{" "}
             <i className=" text-danger">@{user.username}</i>
           </h3>
-          
-          <div className="card mt-3 ">
-            <div className="card-body">
-              <h5 className="card-title text-primary">First Blog</h5>
-              <p className="card-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe iste laboriosam illum aspernatur voluptates qui placeat voluptatibus doloribus voluptatum harum autem temporibus, ex aliquid pariatur ut modi, voluptate dolor eum!
-              </p>
-            </div>
-          </div>
-
-          <div className="card mt-3">
-            <div className></div>
-            <div className="card-body">
-              <h5 className="card-title text-primary">Second Blog</h5>
-              <p className="card-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod magni qui quaerat minima rerum hic accusamus quisquam asperiores fugiat quo? Perspiciatis fugiat reiciendis ab quae qui cumque illum fuga quasi!
-              </p>
-            </div>
-          </div>
-          <div className="card mt-3">
-            <div className></div>
-            <div className="card-body">
-              <h5 className="card-title text-primary">Third Blog</h5>
-              <p className="card-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod magni qui quaerat minima rerum hic accusamus quisquam asperiores fugiat quo? Perspiciatis fugiat reiciendis ab quae qui cumque illum fuga quasi!
-              </p>
-            </div>
+          <div className="container my-3 py-3 border border-1">
+            <h4>All Posts</h4>
+            <ul className="list-group">
+              <li className="list-group-items border border-1 d-flex justify-content-between align-items-center rounded-0 p-3 my-2">
+                <div className="start d-flex align-items-center">
+                  <div className="thumbnail img-fluid m-2 border border-2 rounded">
+                    <img src="" alt="U" />
+                  </div>
+                  <div className="post-detail">
+                    <h6>Post Title</h6>
+                    <span>Post Date</span>
+                  </div>
+                </div>
+                <div className="last">
+                  <div className="username d-flex gap-1">
+                    <h6>Username</h6>
+                    <div className="profile_pic broder border-2 rounded-circle">
+                      {" "}
+                      <img src={pp} alt="U" className="img-fluid" />
+                    </div>
+                  </div>
+                  <div className="stats d-flex gap-0 p-2">
+                    <div className="comment">
+                      <span className="p-2">4</span>
+                      <i class="fa-regular fa-comment"></i>
+                    </div>
+                    <div className="count">
+                      <span className="p-2">3</span>
+                      <i class="fa-solid fa-chart-simple"></i>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li className="list-group-items border border-1 d-flex justify-content-between align-items-center rounded-0 p-3 my-2">
+                <div className="start d-flex align-items-center">
+                  <div className="thumbnail img-fluid m-2 border border-2 rounded">
+                    <img src="" alt="U" />
+                  </div>
+                  <div className="post-detail">
+                    <h6>Post Title</h6>
+                    <span>Post Date</span>
+                  </div>
+                </div>
+                <div className="last">
+                  <div className="username d-flex gap-1">
+                    <h6>Username</h6>
+                    <div className="profile_pic broder border-2 rounded-circle">
+                      {" "}
+                      <img src={pp} alt="U" className="img-fluid" />
+                    </div>
+                  </div>
+                  <div className="stats d-flex gap-0 p-2">
+                    <div className="comment">
+                      <span className="p-2">4</span>
+                      <i class="fa-regular fa-comment"></i>
+                    </div>
+                    <div className="count">
+                      <span className="p-2">3</span>
+                      <i class="fa-solid fa-chart-simple"></i>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li className="list-group-items border border-1 d-flex justify-content-between align-items-center rounded-0 p-3 my-2">
+                <div className="start d-flex align-items-center">
+                  <div className="thumbnail img-fluid m-2 border border-2 rounded">
+                    <img src="" alt="U" />
+                  </div>
+                  <div className="post-detail">
+                    <h6>Post Title</h6>
+                    <span>Post Date</span>
+                  </div>
+                </div>
+                <div className="last">
+                  <div className="username d-flex gap-1">
+                    <h6>Username</h6>
+                    <div className="profile_pic broder border-2 rounded-circle">
+                      {" "}
+                      <img src={pp} alt="U" className="img-fluid" />
+                    </div>
+                  </div>
+                  <div className="stats d-flex gap-0 p-2">
+                    <div className="comment">
+                      <span className="p-2">4</span>
+                      <i class="fa-regular fa-comment"></i>
+                    </div>
+                    <div className="count">
+                      <span className="p-2">3</span>
+                      <i class="fa-solid fa-chart-simple"></i>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       ) : (
