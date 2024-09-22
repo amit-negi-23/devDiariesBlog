671ee2ff04f7fd0e1393ab17362f14c0bddf3d3d
amit-negi-23 <amitnegiit23@gmail.com>
2024-09-22 22:52:41 +0530

    updatepassword page ui in progress

--- src/components/updatePassword/updatePassword.jsx
+++ src/components/updatePassword/updatePassword.jsx
@@ -1,30 +1,26 @@
-// import Footer from "../common/footer/footer";
-// import NavBar from "../common/navBar/navBar";
 import { Link } from "react-router-dom";
 import "./UpdatePassword.css";
 
 function UpdatePassword() {
   return (
     <>
-      {/* <NavBar /> */}
       <div class="row py-5 ms-0 w-100">
-        <div class="col-lg-7 col-sm-12 my-4">
+        <div class="col-lg-7 col-sm-12 my-4 px-5">
           <h1 className="fw-bolder ps-5 text-black">
             Enter your new password to update the old password
           </h1>
 
           <p className="p-5 pe-4 mt-5 text-secondary">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            cumque suscipit rem nisi impedit rerum ad. Sed voluptatem tempora
-            delectus unde modi harum nesciunt rerum labore ratione? Ex maiores,
-            tempore officia ea tenetur
+            "Please enter your new password to update your old one. Make sure
+            it's at least 8 characters long, includes a mix of letters, numbers,
+            and symbols for better security."
           </p>
         </div>
-        <div class="custom col-lg-5 col-sm-12 my-4 px-1">
-          <form className=" form-container border border-dark p-5 mx-2">
+        <div class="custom col-lg-5 col-sm-12 my-4 px-5">
+          <form className="container upass-form-container border  px-5 py-4">
             <h1 className="heading ps-2 text-center">DevDiaries</h1>
-            <div className="mb-3">
-              {" "}
+            <i class="fa-solid fa-key mb-5 text-center key-icon"></i>
+            <div className="">
               <label for="exampleInputPassword1">Current Password</label>
               <input
                 type="password"
@@ -34,8 +30,7 @@ function UpdatePassword() {
                 placeholder="Enter your password"
               />
             </div>
-            <div className="mb-3">
-              {" "}
+            <div className="">
               <label for="exampleInputPassword1">New Password</label>
               <input
                 type="password"
@@ -45,13 +40,15 @@ function UpdatePassword() {
                 placeholder="Enter your password"
               />
             </div>
-            <label for="exampleInputPassword1">Confirm Password</label>
-            <input
-              type="password"
-              className="form-control"
-              id="ConfirmPassword"
-              placeholder="Confirm your password"
-            />
+            <div className="">
+              <label for="exampleInputPassword1">Confirm Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="ConfirmPassword"
+                placeholder="Confirm your password"
+              />
+            </div>
 
             <button type="submit" className="btn btn-primary button">
               Submit
@@ -68,7 +65,6 @@ function UpdatePassword() {
           </form>
         </div>
       </div>
-      {/* <Footer /> */}
     </>
   );
 }
