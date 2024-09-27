181c5a719955715b4a5606ace6fe8fdea38f0d07
amit-negi-23 <amitnegiit23@gmail.com>
2024-09-27 10:25:03 +0530

    dummy image added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -57,7 +57,7 @@ function Home() {
                 <div className="posts-list">
                   {blogPosts.map(post => (
                     <article key={post.id} className="post">
-                      <img src={`https://source.unsplash.com/400x200/?${post.id}`} alt={`Post ${post.id}`} />
+                      <img src={`https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D`} alt={`Post ${post.id}`} />
                       <div className="post-content">
                         <h3>{post.title}</h3>
                         <p>{post.body}</p>
