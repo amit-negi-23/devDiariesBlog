8ed6bd657fd6811364648fa3558f224b7791876e
amit-negi-23 <amitnegiit23@gmail.com>
2024-09-27 10:14:40 +0530

    news server added

--- src/components/blogsPage/BlogPage.jsx
+++ src/components/blogsPage/BlogPage.jsx
@@ -11,14 +11,14 @@ export default function BlogPage({ isLayout }) {
   
 
   useEffect(() => {
-    const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=b6274bdcf4c54bd5ac8fb40e8f768a1a&page=1&pageSize=8`;
+    const url = `https://raw.githubusercontent.com/amit-negi-23/NewsServer/refs/heads/main/news.json`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
         return (
           console.log(data),
-          setData(data.articles.slice(0, 2)),
-          setSideArticle(data.articles.toSpliced(0, 2))
+          setData(data[category].articles.slice(0, 2)),
+          setSideArticle(data[category].articles.toSpliced(0, 2))
         );
       });
   }, [category]);
