2942540bb1c179a1280d279280b753f38f0c1259
amit-negi-23 <amitnegiit23@gmail.com>
2024-09-28 12:11:56 +0530

    notfound page design

--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -1,7 +1,17 @@
+import "./NotFound.css"
+import { Link } from "react-router-dom";
+
 function NotFound() {
   return (
     <>
-      <h1>404 not found</h1>
+      <div class="container text-center d-flex justify-content-center align-items-center vh-100">
+    <div class="content">
+      <h1 class="display-1 fw-bold">404</h1>
+      <p class="fs-2"> <span class="text-neon">Oops!</span> Page not found.</p>
+      <p class="lead mb-4">The page you are looking for doesn't exist or has been moved.</p>
+      <Link to="/" class="btn btn-neon btn-lg rounded-pill shadow-lg">Go Back Home</Link>
+    </div>
+  </div>
     </>
   );
 }
