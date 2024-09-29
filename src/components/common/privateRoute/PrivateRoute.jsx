e81dab5f03e14aaf90d5753938bf110ea433ece1
amit-negi-23 <amitnegiit23@gmail.com>
2024-09-29 14:21:51 +0530

    remove comment from PrivateRoute.jsx

--- src/components/common/privateRoute/PrivateRoute.jsx
+++ src/components/common/privateRoute/PrivateRoute.jsx
@@ -5,49 +5,16 @@ function PrivateRoute({ children }) {
   const {
     store: { user },
   } = useAppContext();
+
   // console.log("Private route hit");
-  let decisionInput = useLocation();
+
   if (!user.isLogin) {
-    console.log("user.isLogout hit");
-
-    // switch (decisionInput.pathname) {
-    //   case "/":
-    //   case "/home":
-    //   case "/login":
-    //   case "/register":
-    //   case "/forgotpassword":
-    //     return children;
-    //   default:
-        return <Navigate to="/" />;
-    // }
-
-    // const validPath = ["/", "/home", "/login", "/register", "/forgotpassword"];
-    // if(validPath.includes(decisionInput.pathname)){
-    //   return children;
-    // }else{
-    //   return <Navigate to="/" />;
-    // }
+    // console.log("user.isLogout hit");
+    return <Navigate to="/" />;
   }
   if (user.isLogin) {
-    console.log("user.isLogin hit");
-
-    // switch (decisionInput.pathname) {
-    //   case "/":
-    //   case "/home":
-    //   case "/login":
-    //   case "/register":
-    //   case "/forgotpassword":
-    //     return <Navigate to={`/userpage/${user.id}`} />;
-    //     default:
-        return children;
-    // }
-
-    // const invalidPath = ["/", "/home", "/login", "/register", "/forgotpassword"];
-    // if(invalidPath.includes(decisionInput.pathname)){
-    //   return <Navigate to={`/userPage/${user.id}`} />;
-    //   }else{
-    //   return children;
-    // }
+    // console.log("user.isLogin hit");
+    return children;
   }
 }
 
